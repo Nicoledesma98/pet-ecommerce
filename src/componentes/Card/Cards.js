3be8c3b534d0a6548197bafc886e101371803862
nicolas ledesma <nico-ledesma98@hotmail.es>
2022-05-25 00:09:10 -0300

    arreglando lo ultimo

--- src/componentes/Card/Cards.js
+++ src/componentes/Card/Cards.js
@@ -5,16 +5,16 @@ function Cards() {
         <div className="container">
             <div className="row">
                 <div className="col-md-3">
-                    <CardItem title={"Collar de perro"} pesos="$800"/>
+                    <CardItem title={"Collar de perro"} pesos="$800" stock={2}/>
                 </div>
                 <div className="col-md-3">
-                    <CardItem title={"Collar de gato"} pesos="$300"/>
+                    <CardItem title={"Collar de gato"} pesos="$300" stock={10}/>
                 </div>
                 <div className="col-md-3">
-                    <CardItem title={"Pretal para perro"} pesos="$500" />
+                    <CardItem title={"Pretal para perro"} pesos="$500"stock={8}/>
                 </div>
                 <div className="col-md-3">
-                    <CardItem title={"Pretal para gato"} pesos="$500" />
+                    <CardItem title={"Pretal para gato"} pesos="$500" stock={6} />
                 </div>
             </div>
         </div>
